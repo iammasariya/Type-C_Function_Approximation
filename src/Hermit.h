be48e541ea18cafcd93f130a4ade24502f495c32
iammasariya <pranavvirmasariya@gmail.com>
2017-04-19 22:34:39 -0700

    hermit.h skeleton

---
+++ src/Hermit.h
@@ -0,0 +1,15 @@
+/*
+ * Hermit.h
+ *
+ *  Created on: Apr 19, 2017
+ *      Author: pranav
+ */
+
+#ifndef SRC_HERMIT_H_
+#define SRC_HERMIT_H_
+
+double getHermitPolynomial(float x,int order){
+
+}
+
+#endif /* SRC_HERMIT_H_ */
