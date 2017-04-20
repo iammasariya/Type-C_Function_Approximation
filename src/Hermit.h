4e8bd173458ddeba35823657d12c7057ecf61bd3
iammasariya <pranavvirmasariya@gmail.com>
2017-04-19 23:19:44 -0700

    Changed definition

--- src/Hermit.h
+++ src/Hermit.h
@@ -8,7 +8,7 @@
 #ifndef SRC_HERMIT_H_
 #define SRC_HERMIT_H_
 
-double hermite_next(int n, double x, double Hn, double Hnm1);
-double hermite_imp(int n, double x);
+double hermit_next(int n, double x, double Hn, double Hnm1);
+double hermit_imp(int n, double x);
 
 #endif /* SRC_HERMIT_H_ */
