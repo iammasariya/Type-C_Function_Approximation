206a7eff6b006fcfec55994cc37b1f3d7ead96b7
Gaurav <gauravchodwadia@gmail.com>
2017-05-04 22:44:01 -0700

    basis

--- src/Basis.cpp
+++ src/Basis.cpp
@@ -1,4 +1,4 @@
-
+#include"Basis.h"
 #include<Hermit.h>
 #include<stdio.h>
 
@@ -7,45 +7,61 @@
 using namespace std;
 
 
-double phi(int n,vector<double> featureVector)
+vector<double> phi(vector<double> featureVector)
 {
 
 
 	int length = featureVector.size();
-	double phiValue = 1;
-
-	if(n=1)
-	{
-		//		for(auto i= featureVector.begin();i!=featureVector.end();i++)
-		//		{
-		//
-		//			phiValue = phiValue * hermit_imp(0,i);
-		//		}
-		phiValue=1;
-	}
-
-
-
-	else if(n<=length+1)
-	{
-		int counter=1;
-//		for(auto i= featureVector.begin();i!=featureVector.end();i++)
-//		{
+//	double phiValue = 1;
 //
-//			if()
-//			phiValue = phiValue * hermit_imp(0,i);
-//			counter++;
-//		}
-		phiValue = hermit_imp(1,featureVector[n-2]);
-	}
+//	if(n=1)
+//	{
+//		phiValue=1;
+//	}
+//
+//
+//
+//	else if(n>1 && n<=length+1) //  1st order N element
+//	{
+//		phiValue = hermit_imp(1,featureVector[n-2]);
+//	}
+//
+//
+//	else if(n>length+1 && n<=2*length) // n-1 element
+//	{
+//		phiValue = hermit_imp(1,featureVector[0])*hermit_imp(1,featureVector[n-length-1]);
+//	}
+//
+//	else if(n>2*length && n<= 3*length-1) // 2nd -1st // n-2 elements
+//	{
+//		phiValue = hermit_imp(1,featureVector[1])*hermit_imp(1,featureVector[2*n-length-1]);
+//	}
+//
+//	int div = n/length;
+//	else if(div)
+//	{
+//
+//	}
+//
+//	phiValue = hermit_imp(1,featureVector[div-1])*hermit_imp(1,featureVector[n%length]);
 
 
-	else if(n<=2*length)
-	{
-		phiValue = hermit_imp(1,featureVector[0])*hermit_imp(1,featureVector[n-length-1]);
-	}
+	vector<double> phiValue;
+
+	phiValue.push_back(0);
+
 
 	return phiValue;
 }
 
+
+int main()
+{
+
+	vector<double> featureVector;
+
+	featureVector.push_back(0);
+
+	phi(featureVector);
+}
 
---
+++ src/Basis.h
@@ -0,0 +1,13 @@
+/*
+ * Hermit.h
+ *
+ *  Created on: Apr 19, 2017
+ *      Author: pranav
+ */
+
+#ifndef SRC_BASIS_H_
+#define SRC_BASIS_H_
+
+double phi(int n,vector<double> featureVector);
+
+#endif /* SRC_BASIS_H_ */
